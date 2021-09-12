0328e88f8f4331fccde0b56cf46d8d42ee103b09
Lubov <lb.librab14@gmail.com>
2021-09-12 21:35:30 +0300

    form validation

--- main.js
+++ main.js
@@ -16,10 +16,9 @@ let select = function () {
     }
 
     function selectChoose() {
-        let text = this.innerHTML,
-            select = this.closest('.select'),
+        let select = this.closest('.select'),
             currentText = select.querySelector('.select__current');
-        currentText.innerHTML = text;
+        currentText.innerHTML = this.innerHTML;
         select.classList.remove('is-active');
 
     }
@@ -28,3 +27,34 @@ let select = function () {
 
 
 select();
+
+
+(function () {
+    const loginName = document.querySelector("#name"),
+        loginPhone = document.querySelector("#phone"),
+        authButton = document.querySelector(".auth"),
+        errorName = document.querySelector(".right__error-name"),
+        errorPhone = document.querySelector(".right__error-phone");
+
+    function submitForm(evt) {
+        const loginPhoneRegEx = /^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/;
+
+        const isNameValid =
+            loginName.value.length > 3 && loginName.value.trim() !== "";
+        const isPhoneValid = loginPhoneRegEx.test(loginPhone.value);
+        const isFormValid = isNameValid && isPhoneValid;
+
+        if (isFormValid) {
+            loginName.classList.remove("invalid");
+            loginPhone.classList.remove("invalid");
+        } else {
+            evt.preventDefault();
+            loginName.classList.add("invalid");
+            loginPhone.classList.add("invalid");
+            errorPhone.classList.remove("hidden");
+            errorName.classList.remove("hidden");
+        }
+    }
+
+    authButton.addEventListener("click", submitForm);
+})();
